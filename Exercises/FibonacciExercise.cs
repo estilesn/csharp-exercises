6a38c0c68212fe8922f791d130f92af3d8d54150
Cristian <203697709+estilesn@users.noreply.github.com>
2026-03-10 21:11:38 -0500

    add Fibonacci exercise

--- Exercises/FibonacciExercise.cs
+++ Exercises/FibonacciExercise.cs
@@ -5,6 +5,16 @@ namespace Exercises;
 class FibonacciExercise : IExercise
 {
     public void Execute(){
+        Console.WriteLine("Escribe cuantos digitos de Fibonacci deseas:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int next = 1;
+        int previous = 0;
         
+        for (int i = 0; i < n; i++){
+        int temp = next + previous;
+        next = previous;
+        previous = temp;
+        Console.Write(" " + next);
+        }
     }
 }
